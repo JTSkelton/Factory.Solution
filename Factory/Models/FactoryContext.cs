02fe091ea112c0be32948f6840e3d73aa095fdce
Jack <skelton.jt9@gmail.com>
2022-03-26 12:13:58 -0400

    Added some base files

---
+++ Factory/Models/FactoryContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class FactoryContext : DbContext
+  {
+    public DbSet<Engineer> Engineers { get; set; }
+    public DbSet<Machine> Machiens { get; set; }
+    public DbSet<EngineerMachiens> StylistClients { get; set; }
+
+    public FactoryContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
